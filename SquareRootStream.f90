b02596c8f5e622176321e98e181b7f77d236aae2
Tyler Hatch <hatch.tyler@gmail.com>
2023-05-20 23:29:50 -0700

    Update code to use modern fortran and remove unused variables

--- SquareRootStream.f90
+++ SquareRootStream.f90
@@ -1,26 +1,35 @@
-Program SquareRootStream
+program SquareRootStream
 
+  implicit none
   
-  integer      i,j,k
-  character (len=1000) line
-  real                 value
+  integer             :: status
+  character(len=1000) :: line
+  real                :: value
   
+  ! open input and output files
+  open(11,file='STR_OUT.out')
+  open(12,file='SQRTSTR_OUT.out')
   
-  
-  Open(11,file='STR_OUT.out')
-  Open(12,file='SQRTSTR_OUT.out')
+  ! loop through all lines in input file
   do
-    read(11,'(a40,f20.2)',end=100)line,value
-    if (value.lt.0) then
+    read(11,'(a40,f20.2)',iostat=status)line,value
+    
+    if (status < 0) exit
+    if (status > 0) then
+        write(*,*) "Error reading values from file"
+    end if
+    
+    if (value < 0) then
       value=0
     else
       value=value**0.5
     end if
-!    write(12,'(a39,f10.2)')line,value
     write(12,'(a36,f20.2)')line,value
   end do
   
-100 stop  
+  ! close input and output files
+  close(11)
+  close(12)
   
-  end program
+end program
   
